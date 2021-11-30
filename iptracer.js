a49e5c963c02908c0fc2ee5b1c8beadd126d23ae
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2021-11-30 11:58:18 -0500

    Update iptracer.js

--- iptracer.js
+++ iptracer.js
@@ -1,5 +1,17 @@
 
  var iptext = document.getElementById("iptext")
+     var ip = document.getElementById("ip").value;
+if (ip == null) {      
+
+
 $.getJSON('https://json.geoiplookup.io/?callback=?', function(data) {
    iptext.value = JSON.stringify(data, null, 2);
+}
+if (!ip = null)
+ {
+  
+
+$.getJSON('https://json.geoiplookup.io/?ip=' + ip, function(data) {
+   iptext.value = JSON.stringify(data, null, 2);
+ }
 });
